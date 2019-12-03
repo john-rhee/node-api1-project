d7a624714c6824026feb69f86e1dbda5961a2907
Johnnyr81 <emailjohn81@gmail.com>
2019-12-02 19:04:02 -0600

    update

--- index.js
+++ index.js
@@ -16,14 +16,21 @@ server.get("/", (req, res) => {
 server.post("/api/users", (req, res)=>{
     const reqData = req.body;
 
-    db.insert(reqData)
-    .then(para => {
-        res.status(201).json(para);
-    })
-    .catch(err => {
-        console.log("error on POST /api/users", err);
-        res.status(500).json({ error: "There was an error while saving the user to the database"  });
-    });
+if (!reqData.name || !reqData.bio) {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user."  });  
+    }
+
+else
+    {
+            db.insert(reqData)
+        .then(para => {
+            res.status(201).json(para);
+        })
+        .catch(err => {
+            console.log("error on POST /api/users", err);
+            res.status(500).json({ error: "There was an error while saving the user to the database"  });
+        });
+    }
 });
 
 //GET
@@ -35,7 +42,7 @@ server.get("/api/users", (req, res)=>{
     })
     .catch(error => {
         console.log("error on GET /api/users", error);
-        res.status(500).json({errorMessage: "error getting list of users from database"})
+        res.status(500).json({error: "The users information could not be retrieved."})
     })
 });
 
@@ -45,21 +52,27 @@ server.get("/api/users/:id", (req, res)=>{
 
     const id = req.params.id;
 
-    db.findById(id)
-    .then(database => {
-        res.status(200).json(database);
-    })
-    .catch(error => {
-        console.log("error on GET /api/users/:id", error);
-        res.status(500).json({errorMessage: "error getting a user from database"})
-    })
+            db.findById(id)
+            .then(database => {
+              if (database) {
+                res.status(200).json(database);
+              }
+              else{
+                res.status(404).json({ message: "The user with the specified ID does not exist."  });  
+              }
+            })
+            .catch(error => {
+                console.log("error on GET /api/users/:id", error);
+                res.status(500).json({ error: "The user information could not be retrieved."})
+            })
+       
 });
 
 //DELETE BY ID
 
 server.delete("/api/users/:id", (req, res) => {
     const id = req.params.id;
-  
+
     db.remove(id)
       .then(removed => {
         if (removed) {
@@ -81,17 +94,28 @@ server.put("/api/users/:id", (req, res)=>{
     const updateData = req.body;
     const id = req.params.id;
 
+if (updateData.name && updateData.bio) {   
+
     db.update(id, updateData)
     .then(para => {
+        if (para) {
         res.status(200).json(para);
+        }
+        else {
+        res.status(404).json({ message: "The user with the specified ID does not exist."  });
+        }
     })
     .catch(err => {
         console.log("error on PUT /api/users/:id", err);
         res.status(500).json({ error: "The user information could not be modified."  });
     });
-}); 
+  }
 
- 
+else  {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user."  });  
+    }
+
+}); 
 
 const port = 5000;
 server.listen(port,()=>
